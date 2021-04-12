673a8c7b402fbc40e6a1f65e8f54d19a904df48f
Andriy Balan <81969926+Andriy099@users.noreply.github.com>
2021-04-12 07:35:36 +0200

    Add files via upload

---
+++ assets/js/clipboard.js
@@ -0,0 +1,10 @@
+function copy(){
+    navigator.clipboard.writeText('mc.MagnetoMC.eu')
+    swal({
+    title: 'IP COPIATO',
+    text: 'BUON GIOCO',
+    icon: 'success',
+    timer: 2000,
+    buttons: false,
+})
+}
